2df5af1ef4aaff466d69ea265d30231522516ae3
darciu <giemzadariusz@gmail.com>
2023-02-22 01:42:21 +0100

    entity chords have now different attributes

--- src/entities/chords.py
+++ src/entities/chords.py
@@ -1,27 +1,56 @@
 from dataclasses import dataclass
 from typing import Dict, List
-from .structures.chords import all_chords
+from structures import all_chords
 
 
 
 @dataclass
 class Chords:
-    """A class holding chords in dictionary in form:
+    """A class holding chords in dictionaries in form:
     chord type (str) : list of integer steps
 
     where steps are following building chord pitches
     eg. "major": [0,4,7]
 
-    Attributes:
+    Attributes
+    -----------
     all: dict
         all chords stored as dictionary
+    minor: dict
+        minor third chords stored as dictionary
+    major: dict
+        major third chords stored as dictionary
+    dimished_fifth: dict
+        dimished_fifth third chords stored as dictionary
+    perfect_fifth: dict
+        perfect_fifth third chords stored as dictionary
+    augmented_fifth: dict
+        augmented_fifth third chords stored as dictionary
+    minor_seventh: dict
+        minor_seventh third chords stored as dictionary
+    major_seventh: dict
+        major_seventh third chords stored as dictionary
     """
     all: Dict[str, List[int]]
+    minor: Dict[str, List[int]]
+    major: Dict[str, List[int]]
+    dimished_fifth: Dict[str, List[int]]
+    perfect_fifth: Dict[str, List[int]]
+    augmented_fifth: Dict[str, List[int]]
+    minor_seventh: Dict[str, List[int]]
+    major_seventh: Dict[str, List[int]]
 
     @staticmethod
     def load() -> "Chords":
         """Create Chords class object from static dictionary"""
 
-        all = {name: [int(step) for step in struct.split(',')] for name, struct in all_chords.items()}
+        all = {name: struct for name, struct in all_chords.items()}
+        minor = {name: struct for name, struct in all_chords.items() if 3 in struct}
+        major = {name: struct for name, struct in all_chords.items() if 4 in struct}
+        dimished_fifth = {name: struct for name, struct in all_chords.items() if 6 in struct}
+        perfect_fifth = {name: struct for name, struct in all_chords.items() if 7 in struct}
+        augmented_fifth = {name: struct for name, struct in all_chords.items() if 8 in struct}
+        minor_seventh = {name: struct for name, struct in all_chords.items() if 10 in struct}
+        major_seventh = {name: struct for name, struct in all_chords.items() if 11 in struct}
 
-        return Chords(all)
+        return Chords(all, minor, major, dimished_fifth, perfect_fifth, augmented_fifth, minor_seventh, major_seventh)
--- src/entities/structures.py
+++ src/entities/structures.py
@@ -1,3 +1,22 @@
+all_chords = {
+    "major": [0,4,7],
+    "minor": [0,3,7],
+    "dominant7": [0,4,7,10],
+    "m7": [0,3,7,10],
+    "maj7": [0,4,7,11],
+    "m7b5": [0,3,6,10],
+    "dominant7_plus5": [0,4,8,10],
+    "dominant7_b5": [0,4,6,10],
+    "sus2": [0,2,7],
+    "sus4": [0,5,7],
+    "m7_plus5": [0,3,8,10],
+    "maj7_b5": [0,4,6,11],
+    "m_maj7": [0,3,7,11],
+    "dimished": [0,3,6,9],
+    "augmented": [0,4,8],
+    "maj7_plus5":[0,4,8,11]
+}
+
 all_scales = {
     "modal_scales": {
         "ionian": {
@@ -42,5 +61,79 @@ all_scales = {
         "blues_minor": "0,3,5,6,7,10",
         "blues_major": "0,2,3,4,7,9",
         "mixo_blues": "0,2,3,4,5,6,8,9,11"
+    }
+}
+
+all_harmonies = {
+    "major": {
+        "progression": [
+            "maj7",
+            "m7",
+            "m7",
+            "maj7",
+            "dominant7",
+            "m7",
+            "m7b5"
+        ],
+        "steps": 
+            "0,2,4,5,7,9,11"
+        
+    },
+    "minor": {
+        "progression": [
+            "m7",
+            "m7b5",
+            "maj7",
+            "m7",
+            "m7",
+            "maj7",
+            "dominant7"
+        ],
+        "steps": 
+            "0,2,3,5,7,8,10"
+        
+    },
+    "harmonic_minor": {
+        "progression": [
+            "m_maj7",
+            "m7b5",
+            "maj7_plus5",
+            "m7",
+            "dominant7",
+            "maj7",
+            "dimished"
+        ],
+        "steps": 
+            "0,2,3,5,7,11"
+        
+    },
+    "melodic_minor": {
+        "progression": [
+            "m_maj7",
+            "m7",
+            "maj7_plus5",
+            "dominant7",
+            "dominant7",
+            "m7b5",
+            "m7b5"
+        ],
+        "steps": 
+            "0,2,3,5,7,9,11"
+        
+    },
+    "double_harmonic_minor": {
+        "progression": [
+            "minor",
+            ""
+        ],
+        "steps": ""
+    },
+    "double_harmonic_major": {
+        "progression": [],
+        "steps": ""
+    },
+    "harmonic_major": {
+        "progression": [],
+        "steps": ""
     }
 }
