2105ac4a34078730978a3661d8989770b3fae79d
darciu <giemzadariusz@gmail.com>
2023-03-21 15:57:40 +0100

    added chords endpoints tests

--- src/tests/test_endpoints.py
+++ src/tests/test_endpoints.py
@@ -1,7 +1,22 @@
+from itertools import combinations
 from fastapi.testclient import TestClient
+import pytest
+
 from main import app
+from entities.chords import Chords
+
+@pytest.fixture
+def scales():
+    return Scales.load()
+
+@pytest.fixture
+def chords():
+    return Chords.load()
 
 client = TestClient(app)
+
+
+# POST ENDPOINTS
 
 def test_multiple_scales_multiple_chords_response():
     response = client.post('v1/multiple_scales_multiple_chords', json = {"repeat_n_times":1})
@@ -23,3 +38,28 @@ def test_random_background_chords_response():
 def test_random_scales_one_chord_response():
     response = client.post('v1/random_scales_one_chord', json = {"repeat_n_times":1})
     assert response.status_code == 200
+
+
+# GET ENDPOINTS
+
+## CHORDS
+
+def test_chord_by_name(chords):
+    for chord_name in chords.all.keys():
+        response = client.get(f'v1/chords/chord_by_name/{chord_name}')
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+
+def test_all_chords_names_response():
+    response = client.get('v1/chords/all_chords_names')
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+param_input_filter_chords = combinations(['major', 'minor', 'dimished_fifth', 'perfect_fifth', 'augmented_fifth', 'minor_seventh', 'major_seventh'], 2)
+
+@pytest.mark.parametrize("pairs", param_input_filter_chords)
+def test_filter_chords(pairs):
+    response = client.get(f'v1/chords/filter_chords/?chords_types={pairs[0]}&chords_types={pairs[1]}')
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
