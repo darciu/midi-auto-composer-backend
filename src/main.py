0fcb2a601354fe17d8ad71d46ab9f6475992891b
darciu <41596795+darciu@users.noreply.github.com>
2024-01-28 02:01:42 +0100

    pull from main to develop (#20)
    * Develop (#17)
    
    * send mp3 file (#4)
    
    * One scale one chord (#5)
    
    * parameters corrected
    
    * minor changes
    
    * Custom creator (#6)
    
    * changed name and schema
    
    * midi_composer quarternotes fix
    
    * fix tests
    
    * Pretty backing tracks (#7)
    
    * 3/4 metrum percussion done
    
    * metrum 2/4 done
    
    * 4/4 metrum done
    
    * 5/4 metrum done
    
    * 6/4 and 7/4 metrum done
    
    * Build script (#9)
    
    * working script
    
    * pre-commit
    
    exit
    
    * run script
    
    * pre-commit-script
    
    * always run
    
    * output folder
    
    * Merge play modes (#10)
    
    * done
    
    * intervals mode
    
    * difficulty level
    
    * intervals detailed
    
    * schema changes
    
    * done (#15)
    
    Co-authored-by: dgiemza <dariusz.giemza@grupawp.pl>
    
    ---------
    
    Co-authored-by: dgiemza <dariusz.giemza@grupawp.pl>
    
    * Develop (#19)
    
    * send mp3 file (#4)
    
    * One scale one chord (#5)
    
    * parameters corrected
    
    * minor changes
    
    * Custom creator (#6)
    
    * changed name and schema
    
    * midi_composer quarternotes fix
    
    * fix tests
    
    * Pretty backing tracks (#7)
    
    * 3/4 metrum percussion done
    
    * metrum 2/4 done
    
    * 4/4 metrum done
    
    * 5/4 metrum done
    
    * 6/4 and 7/4 metrum done
    
    * Build script (#9)
    
    * working script
    
    * pre-commit
    
    exit
    
    * run script
    
    * pre-commit-script
    
    * always run
    
    * output folder
    
    * Merge play modes (#10)
    
    * done
    
    * intervals mode
    
    * difficulty level
    
    * intervals detailed
    
    * schema changes
    
    * done (#15)
    
    Co-authored-by: dgiemza <dariusz.giemza@grupawp.pl>
    
    * added config (#18)
    
    ---------
    
    Co-authored-by: dgiemza <dariusz.giemza@grupawp.pl>
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * cors policy
    
    * dummy endpoint
    
    * dummy endpoint
    
    * dummy endpoint
    
    * config
    
    * credentials
    
    ---------
    
    Co-authored-by: dgiemza <dariusz.giemza@grupawp.pl>

--- src/main.py
+++ src/main.py
@@ -6,8 +6,17 @@ else:
     FRONTEND_URL = environ.get('FRONTEND_URL')
 
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+username_secret = environ.get('USERNAME','username')
+password_secret = environ.get('PASSWORD','password')
+
+import secrets
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from starlette.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, UJSONResponse, JSONResponse
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
+from fastapi.openapi.utils import get_openapi
+
 
 from api.v1 import custom_creator
 from api.v1 import pattern
@@ -17,6 +26,8 @@ from api.v1 import intervals
 from api.v1 import get_scales
 from api.v1 import get_chords
 from api.v1 import get_scales_chords
+
+security = HTTPBasic()
 
 tags_metadata = [
     {
@@ -36,9 +47,25 @@ tags_metadata = [
 ]
 
 app = FastAPI(title='MIDI Auto Composer'
-              ,openapi_tags=tags_metadata)
+              ,openapi_tags=tags_metadata
+              ,docs_url=None
+              ,redoc_url=None
+              ,openapi_url="/api/openapi.json"
+              ,default_response_class=UJSONResponse)
+
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = f"{FRONTEND_URL}, https://audiotrainer.pl, http://localhost:3333, http://127.0.0.1:3333"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
 
 origins = [
+    FRONTEND_URL,
+    "https://audiotrainer.pl",
     "http://localhost:3333",
     "http://127.0.0.1:3333",
     FRONTEND_URL,
@@ -53,7 +80,6 @@ app.add_middleware(
 )
 
 
-
 app.include_router(pattern.router, prefix="/v1")
 app.include_router(intervals.router, prefix="/v1")
 app.include_router(scales_one_chord.router, prefix="/v1")
@@ -63,4 +89,29 @@ app.include_router(custom_creator.router, prefix="/v1")
 app.include_router(get_scales.router, prefix="/v1/scales")
 app.include_router(get_chords.router, prefix="/v1/chords")
 app.include_router(get_scales_chords.router, prefix="/v1/scales_chords")
+
+@app.get('/dummy')
+def dummy():
+    return environ.get('DUMMY')
+
+
+def get_username(credentials: HTTPBasicCredentials = Depends(security)) -> str:
+    correct_username = secrets.compare_digest(credentials.username, username_secret)
+    correct_password = secrets.compare_digest(credentials.password, password_secret)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@app.get("/docs", response_class=HTMLResponse)
+async def get_docs(username: str = Depends(get_username)):
+    return get_swagger_ui_html(openapi_url="/api/openapi.json", title="docs")
+
+
+@app.get("/redoc", response_class=HTMLResponse)
+async def get_redoc(username: str = Depends(get_username)):
+    return get_redoc_html(openapi_url="/api/openapi.json", title="redoc")
 
